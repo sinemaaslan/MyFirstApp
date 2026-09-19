c7fbfcc3f60dd9dd791bfd678f8b0710050a9988
Sinem Aslan <sinemaslan711@gmail.com>
2026-09-19 16:13:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myfirstapplication
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        }
+
+
+  
+}
