7ae0afef359b02b9622c6bd1acc12b5c8b0199c8
Sinem Aslan <sinemaslan711@gmail.com>
2026-09-20 14:49:49 +0300

    Added third button to change background color

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.myfirstapplication;
-import android.annotation.SuppressLint;
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView myTextView;
+    private Button btnChangeText;
+    private Button btnChangeColor;
+    private Button btnChangeBgColor;
+    private LinearLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +22,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myTextView = findViewById(R.id.myTextView);
-        Button btnChangeText = findViewById(R.id.btnChangeText);
-        Button btnChangeColor = findViewById(R.id.btnChangeColor);
+        btnChangeText = findViewById(R.id.btnChangeText);
+        btnChangeColor = findViewById(R.id.btnChangeColor);
+        btnChangeBgColor = findViewById(R.id.btnChangeBgColor);
+        mainLayout = findViewById(R.id.mainLayout);
 
+        // 1. Button: Changes text
         btnChangeText.setOnClickListener(new View.OnClickListener() {
-
-            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                myTextView.setText("Hello World!");
+                myTextView.setText("Text Changed!");
             }
         });
-        btnChangeColor.setOnClickListener(v -> myTextView.setTextColor(Color.GREEN));
-    }
 
+        // 2. Button: Changes text color to green
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myTextView.setTextColor(Color.GREEN);
+            }
+        });
+
+        // 3. Button: Changes background color to yellow
+        btnChangeBgColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainLayout.setBackgroundColor(Color.YELLOW);
+            }
+        });
+    }
 }
